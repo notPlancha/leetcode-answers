499d8053f15c573eb2d63ff1a107b78612ef3fce
notPlancha <andre.plancha@hotmail.com>
2022-08-27 19:57:12 +0100

    added 13

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from typing import Optional, Iterator, List
 from util import *
 
+
+# noinspection PyMethodMayBeStatic
 class Solution:
-    #387. First Unique Character in a String
+    # 387. First Unique Character in a String
     def firstUniqChar(self, s: str) -> int:
         listOfCaracthers = dict()
         for index, i in enumerate(s):
@@ -17,36 +19,41 @@ class Solution:
             if b["count"] == 1:
                 return b["indexOfFirst"]
         return -1
-    #538. Convert BST to Greater Tree
+
+    # 538. Convert BST to Greater Tree
     def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         def transverseBST(root: TreeNode | None) -> Iterator[TreeNode | None]:
-            NoneRoot = root is not None
-            if NoneRoot: yield from transverseBST(root.right)
+            none_root = root is not None
+            if none_root: yield from transverseBST(root.right)
             yield root
-            if NoneRoot: yield from transverseBST(root.left)
-        valueToSum = 0
+            if none_root: yield from transverseBST(root.left)
+
+        value_to_sum = 0
         for i in transverseBST(root):
             if i is not None:
-                i.val += valueToSum
-                valueToSum = i.val
+                i.val += value_to_sum
+                value_to_sum = i.val
         return root
+
     # 1038. Binary Search Tree to Greater Sum Tree
     def bstToGst(self, root: TreeNode) -> TreeNode:
         return self.convertBST(root)
-    #1. Two Sum
+
+    # 1. Two Sum
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #challenge: not O(n^2)
+        # challenge: not O(n^2)
         hmap = dict()
-        for index, i in enumerate(nums):#O(n)
-            if i not in hmap: #O(1) since it's a hashmap
-                #current_i + future_i = target <=>target-current_i = future_i,
-                #so this will record the index of current i to get when future_i appears
-                hmap[target-i] = index
+        for index, i in enumerate(nums):  # O(n)
+            if i not in hmap:  # O(1) since it's a hashmap
+                # current_i + future_i = target <=>target-current_i = future_i,
+                # so this will record the index of current i to get when future_i appears
+                hmap[target - i] = index
             else:
-                #this i is now future_i
+                # this i is now future_i
                 return [hmap[i], index]
         return [-1, -1]
-    #7. Revserse Integer
+
+    # 7. Revserse Integer
     def reverse(self, x: int, outsideRange=True) -> int:
         rev = 0
         if x < 0:
@@ -64,14 +71,16 @@ class Solution:
             # region remove last digit from x
             x //= 10
             # endregion9
-        if outsideRange and rev > (2**31) - 1: return 0
+        if outsideRange and rev > (2 ** 31) - 1: return 0
         return -rev if neg else rev
-    #9. Palindrome Number
-    def isPalindrome(self,  x: int) -> bool:
+
+    # 9. Palindrome Number
+    def isPalindrome(self, x: int) -> bool:
         if x < 0: return False
-        #Challenge: Could you solve it without converting the integer to a string?
+        # Challenge: Could you solve it without converting the integer to a string?
         return x == self.reverse(x, False)
-    #2. Add Two Numbers
+
+    # 2. Add Two Numbers
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         firstNode = ret = ListNode()
         oneMore, lastNumber = False, None
@@ -85,14 +94,14 @@ class Solution:
                 oneMore = False
             l1, l2 = l1.next, l2.next
             if None in [l1, l2]:
-                #one list reached the end
+                # one list reached the end
                 break
-            #both lists still have more numbers
-            ret.next= ListNode()
+            # both lists still have more numbers
+            ret.next = ListNode()
             ret = ret.next
         notFinished = l2 if l1 is None else l1
         if notFinished is None:
-            #both reached the end, which means they are of same size, which means only 1 can be aside
+            # both reached the end, which means they are of same size, which means only 1 can be aside
             if oneMore: ret.next = ListNode(1)
             return firstNode
         while True:
@@ -109,36 +118,38 @@ class Solution:
                 return firstNode
             else:
                 ret, notFinished = ret.next, notFinished.nextx
-    #1338. Reduce Array Size to The Half
-    def minSetSize(self, arr: List[int]) -> int: #TODO refazer q a lista está infinita
-        halfArr = len(arr)//2
+
+    # 1338. Reduce Array Size to The Half #unfinished
+    def minSetSize(self, arr: List[int]) -> int:  # TODO refazer q a lista está infinita
+        halfArr = len(arr) // 2
         if halfArr == 0: return halfArr
+
         class Node:
-            def __init__(self, val, prev= None, next = None):
+            def __init__(self, val, prev=None, next=None):
                 self.val = val
                 self.prev = prev
                 self.next = next
 
-        #first is biggest, last is smallest
+        # first is biggest, last is smallest
         mapp: dict[int, Node] = dict()
         mapp[arr[0]] = lastNode = firstNode = Node(1)
         for i in arr[1:]:
             n = mapp.get(i)
             if n is None:
-                #region new number found, so make n the last because uts the smallest
+                # region new number found, so make n the last because uts the smallest
                 n = Node(1, prev=lastNode)
                 mapp[i] = n
                 lastNode.next, lastNode = n, n
-                #endregion
+                # endregion
             else:
-                #region get node of number previous seen and increment the seen times TODO wrong
+                # region get node of number previous seen and increment the seen times TODO wrong
                 n.val += 1
                 if n.val == 4:
                     pass
-                #endregion
-                #region bubble new priority number to right palce to have the queue ordered
+                # endregion
+                # region bubble new priority number to right palce to have the queue ordered
                 while n.prev is not None and n.prev.val < n.val:
-                    #[A] <-> B <-> n <-> [D] TO [A] <-> n <-> B <-> [D]
+                    # [A] <-> B <-> n <-> [D] TO [A] <-> n <-> B <-> [D]
                     B, D = n.prev, n.next
                     A = B.prev
                     if A is not None:
@@ -147,10 +158,10 @@ class Solution:
                     B.prev, B.next = n, D
                     if D is not None:
                         D.prev = B
-                #endregion
+                # endregion
                 if n.prev is None: firstNode = n
         ret = 0
-        #TODO linnked list is on infinite loop
+        # TODO linnked list is on infinite loop
         while True:
             if firstNode.next is None:
                 break
@@ -162,6 +173,7 @@ class Solution:
             halfArr -= firstNode.val
             if halfArr <= 0: return ret
             firstNode = firstNode.next
+
     def minSetSizeLazy(self, arr: List[int]) -> int:
         mapp = dict()
         for i in set(arr):
@@ -170,10 +182,36 @@ class Solution:
         s = sorted(mapp.values(), reverse=True)
         for index, i in enumerate(s):
             halfArr -= i
-            if halfArr <= 0: return index+1
+            if halfArr <= 0: return index + 1
         return 0
+
+    #13. Roman to Integer
+    def romanToInt(self, s: str) -> int:
+        values = {
+            'M': 1000,
+            'D': 500,
+            'C': 100,
+            'L': 50,
+            'X': 10,
+            'V': 5,
+            'I': 1
+        }
+        ret = values[s[0]]
+        last = s[0]
+        for i in s[1:]:
+            ret += values[i]
+            if values[i] > values[last]:
+                ret -= (values[last] * 2) #*2 because the last one was summed too
+            last = i
+        return ret
+
 if __name__ == "__main__":
     a = Solution()
+    """
     l = [3, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    print(a.minSetSize([2,28,92,30,100,52,28,48,91,27,66,19,11,53,91,95,74,51,65,65,96,81,21,55,98,3,2,89,99,57,78,34,50,2,57,76,23,90,89,36,53,22,73,59,95,45]))
+    print(a.minSetSize(
+        [2, 28, 92, 30, 100, 52, 28, 48, 91, 27, 66, 19, 11, 53, 91, 95, 74, 51, 65, 65, 96, 81, 21, 55, 98, 3, 2, 89,
+         99, 57, 78, 34, 50, 2, 57, 76, 23, 90, 89, 36, 53, 22, 73, 59, 95, 45]))
     print(14)
+    """
+    print(a.romanToInt("III"))
